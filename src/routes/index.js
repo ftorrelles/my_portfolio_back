70aba5ff11ac2774f629205a908de29f36f74ab8
ftorrelles <torrellesf93@gmai.com>
2023-03-09 14:49:13 -0400

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const sendEmail = require("../utils/sendEmail");
 
 // colocar las rutas aquí
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
     res.send("Welcome to express");
 });
 
+router.post("/emails/contact", async (req, res) => {
+    const { name, email, comment } = req.body;
+    await sendEmail({
+        to: "torrellesf93@gmail.com",
+        subject: "Mensaje del protafolio",
+        html: ` 
+				<h1>Hi Francisco, someone wrote to you from your portfolio.</h1>
+                <br>
+                <br>
+                <p><span>Name: </span>${name}</p>
+                <p><span>Email: </span>${email}</p>
+                <br>
+                <p><span>comment: </span>${comment}</p>
+				
+            `,
+    });
+    return res.json("Email sent succesfully");
+});
 
 module.exports = router;
